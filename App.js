35cbe6e94d379d13761cef37e97a876ba8acc44f
NandiniJagani <nandinijagani10@gmail.com>
2024-03-07 09:42:35 +0530

    NJ | changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Nandini</Text>
+      <Text>Nandini12345668900</Text>
       <StatusBar style="auto" />
     </View>
   );
