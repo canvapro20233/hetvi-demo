74573b2cb48b4df27028f987990afecd13ea45ef
PK <palakkathiriya3@gmail.com>
2024-03-07 10:37:37 +0530

    PK | changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hetvi</Text>
+      <Text>palak kathiriya</Text>
       <StatusBar style="auto" />
     </View>
   );
