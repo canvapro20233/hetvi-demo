576b6b284b48e22f7640f1b155d42115314d9a7c
HB <boradhetvi214@gmail.com>
2024-03-09 11:30:39 +0530

    HB | new file

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hetvi</Text>
+      <Text>hetvi1326367859ogjfhgytrf67u</Text>
       <StatusBar style="auto" />
     </View>
   );
