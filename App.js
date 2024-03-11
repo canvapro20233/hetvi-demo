d4a1410d2af5cb3cb7ca5187f656ae59fe2eb007
HB <boradhetvi214@gmail.com>
2024-03-11 16:28:18 +0530

    hetvi change app

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ export default screens
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
+//     backgroun
 //   },
 // });
